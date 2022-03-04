77f89b4c6c5bf326602f8f0359ea578fae54631f
SermadMehdi <98916275+SermadMehdi@users.noreply.github.com>
2022-03-04 12:54:25 +0500

    Add files via upload

---
+++ Lab 2/2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <bitset>
+using namespace std;
+
+#define read(n1, n2)          
+    
+    cout << "Please enter num1: "; 
+    cin >> n1;                     
+    cout << "Please enter num2: "; 
+    cin >> n2
+#define fun(x1, x2) (cout << "The answer is: " << x1 * (2 << (x2 - 1)))
+
+using namespace std;
+int main()
+{
+    int num1, num2;
+    read(num1, num2);
+    fun(num1, num2);
+
+    return 0;
+}
